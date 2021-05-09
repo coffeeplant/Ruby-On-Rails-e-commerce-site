1e85bb5899b33f4f73755f7e17173e77a702127c
EC2 Default User <ec2-user@ip-172-31-30-107.ec2.internal>
2021-05-09 20:33:03 +0000

    ready to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Meal.create([
-#     {mealName: 'Lasagne', description: 'Lasagnes description yum', image_url: 'beef.jpeg', category: 'beef', mealAllergens: 'gluten', price: 7},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     {mealName: 'Fish Pie', description: 'Fishy yums', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'fish', price: 8.5},
-#     ])
+Meal.create([
+    {mealName: 'Lasagne', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'beef.jpeg', category: 'beef', mealAllergens: 'Wheat', price: 7},
+    {mealName: 'Fish Pie', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'fish.jpeg', category: 'fish', mealAllergens: 'Fish', price: 8.5},
+    {mealName: 'Chickpea Curry', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'salad.jpeg', category: 'vegan', mealAllergens: 'Soy', price: 8.5},
+    {mealName: 'Eggs Benedict', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'eggs.jpeg', category: 'vegetarian', mealAllergens: 'Egg', price: 8.5},
+    ])
+
+User.create([{email: 'user@email.com', encrypted_password: 'password',prefs: 'Vegan', allergen: 'Wheat', name: 'Bebhinn Hare', address: 'Test Town', phone: '555-1010', role: 'User'},{email: 'staff@email.com', encrypted_password: 'password',prefs: 'Meat', allergen: 'Soy', name: 'Tom Jones', address: 'Test Street', phone: '555-0000', role: 'Staff'},{email: 'admin@email.com', encrypted_password: 'password',prefs: 'Fish', allergen: '', name: 'Lind Desmond', address: 'Test Lane', phone: '555-6666', role: 'Admin'},
+    ])
+    
+    
